3f1e397beb8897dde8b4c7e7c0f1972de917cab1
Ion Vornovitchi <ariathvs@gmail.com>
2024-05-22 00:58:23 +0300

    added phrase top count logic

--- src/main/java/org/task/OutputRunner.java
+++ src/main/java/org/task/OutputRunner.java
@@ -4,8 +4,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class OutputRunner {
 
@@ -16,6 +21,13 @@ public class OutputRunner {
 
         System.out.println("Number of words: " + words.size());
         System.out.println("Number of sentences: " + sentences.size());
+
+        Map<String, Integer> phrases = findPhrases(sentences, 3);
+        Map<String, Integer> topEntriesByValue = getTopEntriesByValue(phrases, 5);
+        for (Map.Entry<String, Integer> entry : topEntriesByValue.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+
     }
 
     public static String getContentFromPath(Path path) {
@@ -38,5 +50,33 @@ public class OutputRunner {
                 .map(String::trim)
                 .filter(sentence -> !sentence.isEmpty())
                 .collect(Collectors.toList());
+    }
+
+    public static Map<String, Integer> findPhrases(List<String> sentences, int phraseSize) {
+        Map<String, Integer> phraseCountMap = new HashMap<>();
+
+        for (String sentence : sentences) {
+            List<String> words = getWords(sentence);
+
+            IntStream.range(0, words.size() - phraseSize + 1)
+                    .mapToObj(i -> String.join(" ", words.subList(i, i + phraseSize)))
+                    .forEach(phrase -> phraseCountMap.merge(phrase, 1, Integer::sum));
+        }
+
+        return phraseCountMap;
+    }
+
+    public static Map<String, Integer> getTopEntriesByValue(Map<String, Integer> map, int n) {
+        return map.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(n)
+                .collect(
+                        Collectors.toMap(
+                                        Map.Entry::getKey,
+                                        Map.Entry::getValue,
+                                        (existing, replacement) -> replacement,
+                                        LinkedHashMap::new
+                        )
+                );
     }
 }
